fc3842e99d6556aae32aaf4dac7ec295a00b55e0
marcof <mfagiani@swissblock.net>
2020-03-11 00:04:33 +0100

    fix roll shift + add param to use same plot

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 COUNTRY_REGION_LIST = ["Switzerland", "Italy"]
-ALIGN_ZERO = False
+ALIGN_ZERO = True
 
 
 def load_data_from_source(source='csse'):
@@ -52,7 +52,7 @@ def clean_data(df, country_region_list=[], align_zero=False):
             n_shift = support_df[col].isna().sum()
 
             if n_shift:
-                support_df[col] = np.roll(support_df[col], len(support_df) - 25)
+                support_df[col] = np.roll(support_df[col], len(support_df) - n_shift)
                 support_df.rename(columns={col: f"{col} (shifted {n_shift} days)"}, inplace=True)
 
         # raise NotImplementedError("Feature to be implemented.")
@@ -61,34 +61,42 @@ def clean_data(df, country_region_list=[], align_zero=False):
     return subset
 
 
-def plot_multi(data, cols=None, spacing=.1, **kwargs):
+def plot_multi(data, cols=None, spacing=.1, same_plot=False, **kwargs):
+    """ref: https://stackoverflow.com/a/11643893/5490538"""
 
-    plt.figure()
+    if same_plot:
+        ax = data.plot(**kwargs)
+        lines, labels = ax.get_legend_handles_labels()
+        ax.legend(lines, labels, loc=2)
 
-    # Get default color style from pandas - can be changed to any other color list
-    if cols is None: cols = data.columns
-    if len(cols) == 0: return
-    # colors = getattr(getattr(plotting, '_matplotlib').style, '_get_standard_colors')(num_colors=len(cols))
-    c_colors = plt.get_cmap("tab10")
-    colors = c_colors.colors
-    # First axis
-    ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
-    ax.set_ylabel(ylabel=cols[0])
-    lines, labels = ax.get_legend_handles_labels()
+    else:
+        plt.figure()
+        # Get default color style from pandas - can be changed to any other color list
+        if cols is None: cols = data.columns
+        if len(cols) == 0: return
+        # colors = getattr(getattr(plotting, '_matplotlib').style, '_get_standard_colors')(num_colors=len(cols))
+        c_colors = plt.get_cmap("tab10")
+        colors = c_colors.colors
+        # First axis
+        ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
+        ax.set_ylabel(ylabel=cols[0])
+        lines, labels = ax.get_legend_handles_labels()
 
-    for n in range(1, len(cols)):
-        # Multiple y-axes
-        ax_new = ax.twinx()
-        ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
-        data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
-        ax_new.set_ylabel(ylabel=cols[n])
+        for n in range(1, len(cols)):
+            # Multiple y-axes
+            ax_new = ax.twinx()
+            ax_new.set_ylabel(ylabel=cols[n])
 
-        # Proper legend position
-        line, label = ax_new.get_legend_handles_labels()
-        lines += line
-        labels += label
+            ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
+            data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
+            # ax_new.set_ylabel(ylabel=cols[n])
 
-    ax.legend(lines, labels, loc=0)
+            # Proper legend position
+            line, label = ax_new.get_legend_handles_labels()
+            lines += line
+            labels += label
+
+        ax.legend(lines, labels, loc=2)
     # plt.show()
     # return ax
 
@@ -102,23 +110,23 @@ if __name__ == '__main__':
     df_recovered = clean_data(df_recovered, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO)
 
     # plot cumulative data
-    plot_multi(df_confirmed,  figsize=(20, 10), title="# of confirmed")
-    plot_multi(df_deaths, figsize=(20, 10), title="# of deaths")
-    plot_multi(df_recovered, figsize=(20, 10), title="# of recovered")
+    plot_multi(df_confirmed,  figsize=(20, 10), title="# of confirmed", same_plot=ALIGN_ZERO)
+    plot_multi(df_deaths, figsize=(20, 10), title="# of deaths", same_plot=ALIGN_ZERO)
+    plot_multi(df_recovered, figsize=(20, 10), title="# of recovered", same_plot=ALIGN_ZERO)
 
     # plot diff from previous day
-    plot_multi(df_confirmed.diff(),  figsize=(20, 10), title="Daily confirmed")
-    plot_multi(df_deaths.diff(), figsize=(20, 10), title="Daily deaths")
-    plot_multi(df_recovered.diff(), figsize=(20, 10), title="Daily recovered")
+    plot_multi(df_confirmed.diff(),  figsize=(20, 10), title="Daily confirmed", same_plot=ALIGN_ZERO)
+    plot_multi(df_deaths.diff(), figsize=(20, 10), title="Daily deaths", same_plot=ALIGN_ZERO)
+    plot_multi(df_recovered.diff(), figsize=(20, 10), title="Daily recovered", same_plot=ALIGN_ZERO)
 
     # plot percentage changes from previous day
-    plot_multi(df_confirmed.pct_change(),  figsize=(20, 10), title="Daily confirmed % change")
-    plot_multi(df_deaths.pct_change(), figsize=(20, 10), title="Daily deaths % change")
-    plot_multi(df_recovered.pct_change(), figsize=(20, 10), title="Daily recovered % change")
+    plot_multi(df_confirmed.pct_change(),  figsize=(20, 10), title="Daily confirmed % change", same_plot=ALIGN_ZERO)
+    plot_multi(df_deaths.pct_change(), figsize=(20, 10), title="Daily deaths % change", same_plot=ALIGN_ZERO)
+    plot_multi(df_recovered.pct_change(), figsize=(20, 10), title="Daily recovered % change", same_plot=ALIGN_ZERO)
 
     # plot percentage changes from cumsum
-    plot_multi(df_confirmed / df_confirmed.cumsum(),  figsize=(20, 10), title="Daily confirmed % over cumsum")
-    plot_multi(df_deaths / df_deaths.cumsum(), figsize=(20, 10), title="Daily deaths % over cumsum")
-    plot_multi(df_recovered / df_recovered.cumsum(), figsize=(20, 10), title="Daily recovered % over cumsum")
+    plot_multi(df_confirmed / df_confirmed.cumsum(),  figsize=(20, 10), title="Daily confirmed % over cumsum", same_plot=ALIGN_ZERO)
+    plot_multi(df_deaths / df_deaths.cumsum(), figsize=(20, 10), title="Daily deaths % over cumsum", same_plot=ALIGN_ZERO)
+    plot_multi(df_recovered / df_recovered.cumsum(), figsize=(20, 10), title="Daily recovered % over cumsum", same_plot=ALIGN_ZERO)
 
     plt.show()
