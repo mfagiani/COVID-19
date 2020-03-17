d14d11d1fcf513aa9d22a1e1b4ef2a3d66ab754d
marcof <m.fagiani85@gmail.com>
2020-03-17 09:26:29 +0100

    stats per 100k inhabitants, daily stats as bars

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import plotly.graph_objs as go
+from plotly.offline import plot
 
 
 FIGSIZE=(20, 10)
 COUNTRY_REGION_LIST = ["Switzerland", "Italy"]
 ALIGN_ZERO = False
+PER_POPULATION = True
+
+POPULATION=dict(
+    Italy=60.48e6,
+    Switzerland=8.57e6
+)
 
 
 def load_data_from_source(source='csse'):
@@ -25,7 +34,7 @@ def load_data_from_source(source='csse'):
     return df_confirmed, df_deaths, df_recovered
 
 
-def clean_data(df, country_region_list=[], align_zero=False):
+def clean_data(df, country_region_list=[], align_zero=False, per_population=False):
 
     if country_region_list:
 
@@ -66,6 +75,12 @@ def clean_data(df, country_region_list=[], align_zero=False):
         support_df.index.name = "Days from first case."
         subset = support_df
 
+    elif per_population:
+        # case per 100k inhabitants
+
+        for col in subset:
+            subset[col] /= POPULATION.get(col) / 1e5
+
     return subset
 
 
@@ -73,9 +88,14 @@ def plot_multi(data, cols=None, spacing=.1, same_plot=False, **kwargs):
     """ref: https://stackoverflow.com/a/11643893/5490538"""
 
     if same_plot:
+        # data.index = data.index.format()
         ax = data.plot(**kwargs)
+        # ax.xaxis.set_major_locator(mdates.DayLocator())
+        # ax.xaxis.set_major_formatter(mdates.DateFormatter('%b - %d'))
         lines, labels = ax.get_legend_handles_labels()
         ax.legend(lines, labels, loc=2)
+        # ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+        # ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
 
     else:
         plt.figure()
@@ -105,36 +125,36 @@ def plot_multi(data, cols=None, spacing=.1, same_plot=False, **kwargs):
             labels += label
 
         ax.legend(lines, labels, loc=2)
-    # plt.show()
-    # return ax
+
+    plt.gcf().autofmt_xdate()
 
 
 if __name__ == '__main__':
 
     df_confirmed, df_deaths, df_recovered = load_data_from_source()
 
-    df_confirmed = clean_data(df_confirmed, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO)
-    df_deaths = clean_data(df_deaths, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO)
-    df_recovered = clean_data(df_recovered, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO)
+    df_confirmed = clean_data(df_confirmed, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO, per_population=PER_POPULATION)
+    df_deaths = clean_data(df_deaths, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO, per_population=PER_POPULATION)
+    df_recovered = clean_data(df_recovered, COUNTRY_REGION_LIST, align_zero=ALIGN_ZERO, per_population=PER_POPULATION)
 
     # plot cumulative data
-    plot_multi(df_confirmed, figsize=FIGSIZE, title="# of confirmed", same_plot=ALIGN_ZERO)
-    plot_multi(df_deaths, figsize=FIGSIZE, title="# of deaths", same_plot=ALIGN_ZERO)
-    plot_multi(df_recovered, figsize=FIGSIZE, title="# of recovered", same_plot=ALIGN_ZERO)
+    plot_multi(df_confirmed, figsize=FIGSIZE, title="# of confirmed (per 100k inhabitants)", same_plot=ALIGN_ZERO or PER_POPULATION, marker='o')
+    plot_multi(df_deaths, figsize=FIGSIZE, title="# of deaths", same_plot=ALIGN_ZERO or PER_POPULATION, marker='o')
+    plot_multi(df_recovered, figsize=FIGSIZE, title="# of recovered", same_plot=ALIGN_ZERO or PER_POPULATION, marker='o')
 
     # plot diff from previous day
-    plot_multi(df_confirmed.diff(),  figsize=FIGSIZE, title="Daily confirmed", same_plot=ALIGN_ZERO)
-    plot_multi(df_deaths.diff(), figsize=FIGSIZE, title="Daily deaths", same_plot=ALIGN_ZERO)
-    plot_multi(df_recovered.diff(), figsize=FIGSIZE, title="Daily recovered", same_plot=ALIGN_ZERO)
+    plot_multi(df_confirmed.diff(),  figsize=FIGSIZE, title="Daily confirmed (per 100k inhabitants)", same_plot=ALIGN_ZERO or PER_POPULATION, kind='bar')
+    plot_multi(df_deaths.diff(), figsize=FIGSIZE, title="Daily deaths", same_plot=ALIGN_ZERO or PER_POPULATION, kind='bar')
+    plot_multi(df_recovered.diff(), figsize=FIGSIZE, title="Daily recovered", same_plot=ALIGN_ZERO or PER_POPULATION, kind='bar')
 
     # plot percentage changes from previous day
-    plot_multi(df_confirmed.pct_change(),  figsize=FIGSIZE, title="Daily confirmed % change", same_plot=ALIGN_ZERO)
-    plot_multi(df_deaths.pct_change(), figsize=FIGSIZE, title="Daily deaths % change", same_plot=ALIGN_ZERO)
-    plot_multi(df_recovered.pct_change(), figsize=FIGSIZE, title="Daily recovered % change", same_plot=ALIGN_ZERO)
+    plot_multi(df_confirmed.pct_change(),  figsize=FIGSIZE, title="Daily confirmed % change", same_plot=ALIGN_ZERO or PER_POPULATION)
+    plot_multi(df_deaths.pct_change(), figsize=FIGSIZE, title="Daily deaths % change", same_plot=ALIGN_ZERO or PER_POPULATION)
+    plot_multi(df_recovered.pct_change(), figsize=FIGSIZE, title="Daily recovered % change", same_plot=ALIGN_ZERO or PER_POPULATION)
 
     # plot percentage changes from cumsum
-    plot_multi(df_confirmed / df_confirmed.cumsum(),  figsize=FIGSIZE, title="Daily confirmed % over cumsum", same_plot=ALIGN_ZERO)
-    plot_multi(df_deaths / df_deaths.cumsum(), figsize=FIGSIZE, title="Daily deaths % over cumsum", same_plot=ALIGN_ZERO)
-    plot_multi(df_recovered / df_recovered.cumsum(), figsize=FIGSIZE, title="Daily recovered % over cumsum", same_plot=ALIGN_ZERO)
+    plot_multi(df_confirmed / df_confirmed.cumsum(),  figsize=FIGSIZE, title="Daily confirmed % over cumsum", same_plot=ALIGN_ZERO or PER_POPULATION)
+    plot_multi(df_deaths / df_deaths.cumsum(), figsize=FIGSIZE, title="Daily deaths % over cumsum", same_plot=ALIGN_ZERO or PER_POPULATION)
+    plot_multi(df_recovered / df_recovered.cumsum(), figsize=FIGSIZE, title="Daily recovered % over cumsum", same_plot=ALIGN_ZERO or PER_POPULATION)
 
     plt.show()
